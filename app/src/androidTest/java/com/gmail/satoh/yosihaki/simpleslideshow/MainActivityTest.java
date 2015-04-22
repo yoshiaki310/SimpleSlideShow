3c173e83983025e78487df736e77d0350eeb488c
Yoshiaki Satoh <yoshiaki.satoh@gmail.com>
2015-04-23 00:25:28 +0900

    junitの設定が無かったので追加 テストクラスを自動生成

---
+++ app/src/androidTest/java/com/gmail/satoh/yosihaki/simpleslideshow/MainActivityTest.java
@@ -0,0 +1,24 @@
+package com.gmail.satoh.yosihaki.simpleslideshow;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by íıèÕ on 2015/04/23.
+ */
+public class MainActivityTest {
+
+    @org.junit.Test
+    public void testOnCreate() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testOnCreateOptionsMenu() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testOnOptionsItemSelected() throws Exception {
+
+    }
+}
