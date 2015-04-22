ddd28c31790f2e96761060cc9476b1c2b2e4482f
Yoshiaki Satoh <yoshiaki.satoh@gmail.com>
2015-04-22 23:10:10 +0900

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -7,8 +7,8 @@ DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
   cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
-  echo y | android update sdk -u -a -t build-tools-20.0.0 &&
-  echo y | android update sdk -u -a -t android-19 &&
+  echo y | android update sdk -u -a -t build-tools-21.1.2 &&
+  echo y | android update sdk -u -a -t android-22 &&
   echo y | android update sdk -u -a -t extra-android-m2repository &&
   echo y | android update sdk -u -a -t extra-google-m2repository &&
   touch $DEPS
